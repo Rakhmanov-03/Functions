55f045e63e3a4bd61fd06abf0657b8ef67fdff6f
Rakhmanov-03 <omadjonrahmonov588@gmail.com>
2023-11-16 21:43:22 +0500

    Functions

---
+++ main.js
@@ -0,0 +1,73 @@
+// masala1
+// function a(){
+//     for(i=1; i<=10; i++){
+//         console.log(i);
+//     }
+// }
+// a();
+// masala2
+// function a(){
+//     for(i=1; i<100; i+=2){
+//         console.log(i);
+//     }
+// }
+// a();
+// masala3
+// function karra(){
+// for(i=1; i<=10; i++){
+//     console.log(i*7);
+// }
+// }
+// karra();
+// masala4
+// function karra_10_gacha(){
+//     for(i=1; i<=10; i++){
+//         for(k=1; k<=10; k++){
+//             console.log(i*k);
+//         }
+//     }
+//     }
+//     karra_10_gacha();
+// masala5
+// function qoshish(){
+//     let jami=0
+//     for (i=1; i<=10; i++){
+//         jami+=i
+//     }
+//     console.log(jami);
+// }
+// qoshish();
+// masala6
+// function fak(){
+//     let jami=1
+//     for(i=1; i<=10; i++){
+//         jami*=i
+//     }
+//     console.log(jami);
+// }
+// fak();
+// masala7
+// function yigindi(){
+//     let jami=0
+//     for(i=10; i<=30; i+=2){
+//         jami+=i
+//     }
+//     console.log(jami);
+// }
+// yigindi();
+// masala8
+// function shkala(){
+//    let Selsiy=+prompt("Selsiyni kiriting")
+//     console.log(Kelvin=Selsiy+273);
+// }
+// shkala();
+// masala9
+// function shkala(){
+//     let Kelvin=+prompt("Kelvinni kiriting")
+// console.log(Selsiy=Kelvin-273);    
+// }
+// shkala();
+
+
+
+
